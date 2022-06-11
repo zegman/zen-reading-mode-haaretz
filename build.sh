1eb8fe5d247c449cef31dfbeb42708c05d89ffdf
zegman <zegman@gmail.com>
2022-06-11 20:34:24 +0300

    Add Firefox support

--- build.sh
+++ build.sh
@@ -14,3 +14,14 @@ FILES="manifest.json script.js"
 rm -f ../BUILD/zen-reader.zip
 zip ../BUILD/zen-reader.zip $FILES
 
+mkdir ../BUILD/firefox-src
+cp $FILES ../BUILD/firefox-src
+
+# Change manifest version from 3 to 2
+sed -i -e 's/"manifest_version": 3/"manifest_version": 2/' ../BUILD/firefox-src/manifest.json
+web-ext build \
+    --overwrite-dest \
+    --source-dir ../BUILD/firefox-src \
+    --artifacts-dir ../BUILD/firefox
+
+
